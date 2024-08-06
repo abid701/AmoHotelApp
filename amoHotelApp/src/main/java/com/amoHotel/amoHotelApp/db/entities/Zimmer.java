9b5c744c2badf06f43ac1fe1510e63708f74865b
abid701 <abid_erfan7@yahoo.com>
2024-08-06 11:33:55 +0200

    Update Zimmer.java

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/entities/Zimmer.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/entities/Zimmer.java
@@ -1,14 +1,8 @@
 package com.amoHotel.amoHotelApp.db.entities;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import jakarta.persistence.*;
-import org.hibernate.annotations.Type;
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "zimmer")
@@ -20,10 +14,10 @@ public class Zimmer implements Serializable {
     private Long zimmerNr;
 
     private String ausstattung;
-    // diese annotaion ist nur für testen, muss entfernen werden
-    @ElementCollection
+   //TODO: diese annotaion ist nur für testen, muss entfernen werden
+    //@ElementCollection
     @Column(name = "details")
-    private List<String> details;
+    private String details;
 
     private String preisProNacht;
 
@@ -46,11 +40,11 @@ public class Zimmer implements Serializable {
         this.ausstattung = ausstattung;
     }
 
-    public List<String> getDetails() {
+    public String getDetails() {
         return details;
     }
 
-    public void setDetails(List<String> details) {
+    public void setDetails(String details) {
         this.details = details;
     }
 
@@ -67,34 +61,10 @@ public class Zimmer implements Serializable {
         final StringBuilder sb = new StringBuilder("Zimmer{");
         sb.append("zimmerNr=").append(zimmerNr);
         sb.append(", ausstattung='").append(ausstattung).append('\'');
-    //    sb.append(", details=").append(details);
-        sb.append(", details=");
-
-        for(String detail : details) {
-            sb.append(detail);
-            sb.append(", ");
-        }
-        sb.append("\b\b");
+        sb.append(", details='").append(details).append('\'');
         sb.append(", preisProNacht='").append(preisProNacht).append('\'');
         sb.append('}');
         return sb.toString();
     }
-
-
-//        public List<String> getDetailsAsList() {
-//            List<String> detailsList = new ArrayList<>();
-//            if (details!= null) {
-//                try {
-//                    JSONArray jsonArray = new JSONArray(details);
-//                    for (int i = 0; i < jsonArray.length(); i++) {
-//                        detailsList.add(jsonArray.getString(i));
-//                    }
-//                } catch (JSONException e) {
-//                    System.out.println("Json not working!");
-//                }
-//            }
-//            return detailsList;
-//        }
-
 
 }
