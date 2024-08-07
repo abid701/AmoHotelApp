b0d9a469833d15de19fdc98aaffd78bd2705ce85
abid701 <abid_erfan7@yahoo.com>
2024-08-07 14:27:32 +0200

    Update homePage.js
    Javascript code for menu animation

--- amoHotelApp/src/main/resources/static/javascript/homePage.js
+++ amoHotelApp/src/main/resources/static/javascript/homePage.js
@@ -1,3 +1,4 @@
+// TODO: Ich könnte nicht Jquery and javascript zusammen in einem file benutzen, wir müssen das nochmal versuchen. 
 function myFunction(x) {
       x.classList.toggle("change");
     }
