6f70098083f68e46135700028a6d9d3403e89475
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-08 16:11:09 +0200

    Update ZimmerController.java
    Added the method getZimmerDetails to the ZimmerController class

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/web/controller/ZimmerController.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/web/controller/ZimmerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class ZimmerController {
@@ -16,9 +18,18 @@ public class ZimmerController {
 
     @GetMapping("/homePage") // localhost:8081/homePage
     public String  showZimmerList (Model model){
-        Iterable<Zimmer> ZimmerList = zimmerService.getAllZimmer();
-        model.addAttribute("zimmerList", ZimmerList);
+        Iterable<Zimmer> zimmerList = zimmerService.getAllZimmer();
+        model.addAttribute("zimmerList", zimmerList);
         return "view/homePage";
+    }
+
+    @GetMapping("/zimmerDetails")
+    public String getZimmerDetail(@RequestParam("zimmerNr") int zimmerNr, Model model) {
+        Zimmer zimmer = zimmerService.findZimmerByNumber(zimmerNr);
+        
+        model.addAttribute("zimmer", zimmer);
+        
+        return "view/zimmerDetails";
     }
 
 }
