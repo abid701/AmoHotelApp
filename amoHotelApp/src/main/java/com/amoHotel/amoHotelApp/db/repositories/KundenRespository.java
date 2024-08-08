b5e97144cfadf495aa7699b789eb15e5d5901191
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-08 16:17:36 +0200

    Update KundenRespository.java

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/repositories/KundenRespository.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/repositories/KundenRespository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KundenRespository extends CrudRepository<Kunden, Long> {
+public interface KundenRespository extends CrudRepository<Kunden, Integer> {
 }
