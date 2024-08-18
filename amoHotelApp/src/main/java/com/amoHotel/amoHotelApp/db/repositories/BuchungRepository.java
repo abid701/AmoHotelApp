99e25e6f2d0cf77858e2f99b67e0dcb79c209f3b
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-18 13:08:27 +0200

    Update BuchungRepository.java

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/repositories/BuchungRepository.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/db/repositories/BuchungRepository.java
@@ -1,10 +1,14 @@
 package com.amoHotel.amoHotelApp.db.repositories;
 
 import com.amoHotel.amoHotelApp.db.entities.Buchung;
+import com.amoHotel.amoHotelApp.db.entities.Zimmer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BuchungRepository extends CrudRepository<Buchung, Integer> {
+    List<Buchung> findAllByZimmer(Zimmer zimmer);
 }
 
