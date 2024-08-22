c9d4f75ee4e2f82a03fd741dd940779a2925051c
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-22 21:08:40 +0200

    Update ZimmerService.java

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/services/ZimmerService.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/services/ZimmerService.java
@@ -1,7 +1,9 @@
 package com.amoHotel.amoHotelApp.services;
 
 import com.amoHotel.amoHotelApp.db.entities.Buchung;
+import com.amoHotel.amoHotelApp.db.entities.Kunden;
 import com.amoHotel.amoHotelApp.db.entities.Zimmer;
+import com.amoHotel.amoHotelApp.db.repositories.BuchungRepository;
 import com.amoHotel.amoHotelApp.db.repositories.ZimmerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,19 +17,22 @@ public class ZimmerService {
     private final ZimmerRepository zimmerRepository;
     @Autowired
     private final BuchungService buchungService;
+    @Autowired
+    private final KundenService kundenService;
 
 
-    public ZimmerService(ZimmerRepository zimmerRepository, BuchungService buchungService){
+    public ZimmerService(ZimmerRepository zimmerRepository, BuchungService buchungService, KundenService kundenService){
         this.zimmerRepository = zimmerRepository;
         this.buchungService = buchungService;
+        this.kundenService = kundenService;
     }
 
     public Iterable<Zimmer> getAllZimmer(){
         return zimmerRepository.findAll();
     }
 
-    public Zimmer getZimmerByNumber(int id){
-        return zimmerRepository.findById(id).orElse(null);
+    public Zimmer getZimmerByNumber(int zimmerNr){
+        return zimmerRepository.findById(zimmerNr).orElse(null);
     }
 
     public List<Zimmer> getAllZimmerByType(String type){
@@ -36,6 +41,19 @@ public class ZimmerService {
 
     public List<Buchung> getBuchungListByZimmer(Zimmer zimmer){
         return buchungService.getBuchungByZimmer(zimmer);
+    }
+
+    // New
+    public Kunden getKundenById(Integer kundenNr) {
+        return kundenService.getKundenById(kundenNr);
+    }
+
+    public Buchung saveBuchungFromZimmer(Buchung buchung) {
+        return buchungService.saveBuchung(buchung);
+    }
+
+    public Kunden saveKundenFromZimmer(Kunden kunden){
+        return kundenService.saveKunden(kunden);
     }
 
 }
