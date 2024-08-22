2594e5b5ba640dc2a95b14ed8d9f281cb6a0cc4a
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-22 21:16:13 +0200

    Update homePage.js

--- amoHotelApp/src/main/resources/static/javascript/homePage.js
+++ amoHotelApp/src/main/resources/static/javascript/homePage.js
@@ -1,3 +1,27 @@
 function myFunction(x) {
       x.classList.toggle("change");
+      var menu = document.getElementById("menu");
+      menu.classList.toggle("show");
     }
+
+document.addEventListener('click', function(event) {
+    var menu = document.getElementById("menu");
+    var threeLineMenu = document.getElementById("three_line_menu");
+    if (!menu.contains(event.target) && !threeLineMenu.contains(event.target)) {
+        menu.classList.remove("show");
+        threeLineMenu.classList.remove("change");
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let zimmerImages = document.querySelectorAll('img[data-zimmerNr]');
+
+    zimmerImages.forEach(function(imgElement) {
+        let zimmerNr = imgElement.getAttribute('data-zimmerNr');
+
+        imgElement.setAttribute("src", "/images/homePagePics/" + zimmerNr + ".jpg");
+    });
+});
