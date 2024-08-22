c7d85dde861dadef160244f050fb605231b33282
Abid Erfan <abid_erfan7@yahoo.com>
2024-08-22 21:09:39 +0200

    Update KundenService.java

--- amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/services/KundenService.java
+++ amoHotelApp/src/main/java/com/amoHotel/amoHotelApp/services/KundenService.java
@@ -2,6 +2,7 @@ package com.amoHotel.amoHotelApp.services;
 
 import com.amoHotel.amoHotelApp.db.entities.Kunden;
 import com.amoHotel.amoHotelApp.db.repositories.KundenRespository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,15 @@ public class KundenService {
 
     public Iterable<Kunden> getAllKunden(){
         return kundenRespository.findAll();
+    }
+
+    // New
+    public Kunden getKundenById(Integer kundenNr) {
+        return kundenRespository.findById(kundenNr).orElse(null);
+    }
+
+    public Kunden saveKunden(Kunden kunden){
+        return kundenRespository.save(kunden);
     }
 }
 
